6b64f5309ce0e51354b2ccc2abdc6c1385d57893
codeinwp <ionut@codeinwp.com>
2015-01-05 11:03:59 -0600

    update plugin `Revive old post Pro Add-on` version 1.4.7

--- wp-content/plugins/tweet-old-post-pro/tweet-old-post-pro.php
+++ wp-content/plugins/tweet-old-post-pro/tweet-old-post-pro.php
@@ -4,11 +4,11 @@
 #     Plugin URI: https://themeisle.com/plugins/tweet-old-post-pro/
 #     Description: This addon enable the pro functions of Revive Old Post plugin.For questions, comments, or feature requests, <a href="http://themeisle.com/contact/">contact </a> us!
 #     Author: ThemeIsle
-#     Version: 1.4.5
+#     Version: 1.4.7
 #     Author URI: https://themeisle.com/
 #     */
 
-define("ROPPROPLUGINPATH", __DIR__);
+define("ROPPROPLUGINPATH", realpath(dirname(__FILE__)));
 define("VERSION_CHECK", TRUE);
 require_once(ROPPROPLUGINPATH."/inc/core-pro.php");
 
