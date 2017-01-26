ab57d058c060481d429180d2a146fcc0d244ea11
Ionut Neagu <ionut.neagu@vertistudio.com>
2017-01-26 13:36:59 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
 					<div class="email">Email us<br /><span><?php echo get_theme_mod('cwp_footer_email'); ?></span></div>
 				</div><!--/contact-->
 			<div class="copyright">
-					© 2016 codeinwp.com. All Rights Reserved. <br />
+					© 2017 codeinwp.com. All Rights Reserved. <br />
 					WordPress logo is Copyright © WordPress.com
 				</div><!--/copyright-->
 			</div><!--/aboutus-->
@@ -165,6 +165,52 @@ jQuery(document).ready(
   pageVariations[chosenVariation]
 );
 console.log(chosenVariation);
-</script>
+
+
+
+jQuery(document).ready(function($) {
+    var filetypes = /\.(zip|exe|dmg|pdf|doc.*|xls.*|ppt.*|mp3|txt|rar|wma|mov|avi|wmv|flv|wav)$/i;
+    var baseHref = '';
+    if (jQuery('base').attr('href') != undefined) baseHref = jQuery('base').attr('href');
+ 
+    jQuery('a').on('click', function(event) {
+      var el = jQuery(this);
+      var track = true;
+      var href = (typeof(el.attr('href')) != 'undefined' ) ? el.attr('href') :"";
+      var isThisDomain = href.match(document.domain.split('.').reverse()[1] + '.' + document.domain.split('.').reverse()[0]);
+      if (!href.match(/^javascript:/i)) {
+        var elEv = []; elEv.value=0, elEv.non_i=false;
+        if (href.match(/^mailto\:/i)) {
+          elEv.category = "email";
+          elEv.action = "click";
+          elEv.label = href.replace(/^mailto\:/i, '');
+          elEv.loc = href;
+        }
+        else if (href.match(filetypes)) {
+          var extension = (/[.]/.exec(href)) ? /[^.]+$/.exec(href) : undefined;
+          elEv.category = "download";
+          elEv.action = "click-" + extension[0];
+          elEv.label = href.replace(/ /g,"-");
+          elEv.loc = baseHref + href;
+        }
+        else if (href.match(/^https?\:/i) && !isThisDomain) {
+          elEv.category = "external";
+          elEv.action = "click";
+          elEv.label = href.replace(/^https?\:\/\//i, '');
+          elEv.non_i = true;
+          elEv.loc = href;
+        }
+        else if (href.match(/^tel\:/i)) {
+          elEv.category = "telephone";
+          elEv.action = "click";
+          elEv.label = href.replace(/^tel\:/i, '');
+          elEv.loc = href;
+        }
+        else track = false;
+        window._fbq.push(['track', 'ViewContent', {type:'external'}]);
+      }
+    })
+})
+        </script>
 </body>
 </html>
